684a99e0e43c59faf52dc192a6f1cdd06bf5e7f8
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-03-18 01:15:40 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (28.37%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -17,11 +17,5 @@ class Solution {
                 matrix[i][(matrix[0].length-1)-j] = temp;
             }
         }
-
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                System.out.print(matrix[i][j]);
-            }
-        }
-}
+    }
 }
