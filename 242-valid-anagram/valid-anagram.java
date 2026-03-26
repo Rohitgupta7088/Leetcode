bffd3a3f338a15d917a054a06b88ce535f929256
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-03-27 00:28:44 +0530

    Time: 4 ms (81.67%) | Memory: 46.5 MB (37.80%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,36 +4,44 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        int arr[] = new int[26];
-        for(int i=0; i<s.length(); i++){
-            if(arr[s.charAt(i) - 'a'] == 0){
-                int count = 0;
-                for(int j=0; j<s.length(); j++){
-                    if(s.charAt(i) == s.charAt(j)){
-                        count ++;
-                    }
-                }
+        // int arr[] = new int[26];
+        // for(int i=0; i<s.length(); i++){
+        //     if(arr[s.charAt(i) - 'a'] == 0){
+        //         int count = 0;
+        //         for(int j=0; j<s.length(); j++){
+        //             if(s.charAt(i) == s.charAt(j)){
+        //                 count ++;
+        //             }
+        //         }
 
-                arr[s.charAt(i) - 'a'] = count;
-            }
-        }
+        //         arr[s.charAt(i) - 'a'] = count;
+        //     }
+        // }
 
-        for(int i=0; i<t.length(); i++){
-            int count = 0;
-            if(arr[t.charAt(i)-'a'] == -1){
-                continue;
-            }
-            for(int j=0; j<t.length(); j++){
-                if(t.charAt(i) == t.charAt(j)){
-                    count ++;
-                }
-            }
-            if(arr[t.charAt(i)-'a'] != count){
-                return false;
-            }
-            arr[t.charAt(i)-'a'] = -1;
-        }
+        // for(int i=0; i<t.length(); i++){
+        //     int count = 0;
+        //     if(arr[t.charAt(i)-'a'] == -1){
+        //         continue;
+        //     }
+        //     for(int j=0; j<t.length(); j++){
+        //         if(t.charAt(i) == t.charAt(j)){
+        //             count ++;
+        //         }
+        //     }
+        //     if(arr[t.charAt(i)-'a'] != count){
+        //         return false;
+        //     }
+        //     arr[t.charAt(i)-'a'] = -1;
+        // }
 
-        return true;
+        // return true;
+
+        char arrs[] = s.toCharArray();
+        char arrt[] = t.toCharArray();
+
+        Arrays.sort(arrs);
+        Arrays.sort(arrt);
+
+        return Arrays.equals(arrs, arrt);
     }
 }
