822f3a2451c0c438f922fef4b80c41e45b12db69
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-03-27 23:11:56 +0530

    Time: 3 ms (87.32%) | Memory: 43.4 MB (35.10%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> ss = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '{' || ch == '(' || ch == '['){
+                ss.push(ch);
+            }
+            else{
+                if(ss.isEmpty()){
+                    return false;
+                }
+
+                if( (ch == '}' && ss.peek() == '{') 
+                        || (ch == ')' && ss.peek() == '(')
+                        || (ch == ']' && ss.peek() == '[')
+                    ){
+                        ss.pop();
+                    }
+                else{
+                    return false;
+                }
+
+            }
+        }
+
+        if(ss.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
