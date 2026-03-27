c70f88e79a9796a6f3f731f5f724f441d5909c36
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-03-27 22:47:11 +0530

    Time: 1 ms (46.48%) | Memory: 42.9 MB (88.94%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -3,27 +3,42 @@ class Solution {
         return helper(haystack, needle);
     }
 
+    // public int helper(String s1, String s2){
+    //     int i=0;
+    //     while(i < s1.length()){
+    //         while(i<s1.length() && s1.charAt(i) != s2.charAt(0)){
+    //             i++;
+    //         }
+    //         int si = i;
+    //         int k = i;
+    //         int j = 0;
+    //         while( j<s2.length() && k<s1.length() && s1.charAt(k) == s2.charAt(j) ){
+    //             k++;
+    //             j++;
+    //         }
+    //         int ei = k;
+    //         if(s2.equals(s1.substring(si,ei)) ){
+    //             return si;
+    //         }
+
+    //         i++;
+    //     }
+
+    //     return -1;
+    // }
+
     public int helper(String s1, String s2){
-        int i=0;
-        while(i < s1.length()){
-            while(i<s1.length() && s1.charAt(i) != s2.charAt(0)){
-                i++;
-            }
-            int si = i;
-            int k = i;
-            int j = 0;
-            while( j<s2.length() && k<s1.length() && s1.charAt(k) == s2.charAt(j) ){
-                k++;
+        int n=s1.length();
+        int m=s2.length();
+        for(int i=0; i<=n-m; i++){
+            int j=0;
+            while(j<m && s1.charAt(i+j) == s2.charAt(j)){
                 j++;
             }
-            int ei = k;
-            if(s2.equals(s1.substring(si,ei)) ){
-                return si;
+            if(j == m){
+                return i;
             }
-
-            i++;
         }
-
         return -1;
     }
 }
