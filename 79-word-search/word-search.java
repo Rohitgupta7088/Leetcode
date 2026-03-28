e682235550a6b1446e90deebc161117e5554a598
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-03-28 23:05:38 +0530

    Time: 140 ms (49.82%) | Memory: 43.1 MB (38.10%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -4,7 +4,7 @@ class Solution {
         int n = board[0].length;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if( helper(board, word, "", i, j, m, n, 0) ){
+                if( helper(board, word, i, j, m, n, 0) ){
                     return true;
                 }
             }
@@ -13,12 +13,10 @@ class Solution {
         return false;
     }
 
-    public boolean helper(char[][] board, String word, String newstr, int i, int j, int m, int n, int k){
+    public boolean helper(char[][] board, String word, int i, int j, int m, int n, int k){
         if( i<0 || j<0 || i>=m || j>=n || board[i][j] != word.charAt(k)){
             return false;
         }
-
-        newstr += board[i][j];
 
         if(k == word.length()-1){
             return true;
@@ -27,10 +25,10 @@ class Solution {
         char temp = board[i][j];
         board[i][j] = '#';
 
-        boolean found = helper(board, word, newstr, i+1, j, m, n, k+1) ||
-                        helper(board, word, newstr, i, j+1, m, n, k+1) ||
-                        helper(board, word, newstr, i-1, j, m, n, k+1) ||
-                        helper(board, word, newstr, i, j-1, m, n, k+1);
+        boolean found = helper(board, word, i+1, j, m, n, k+1) ||
+                        helper(board, word, i, j+1, m, n, k+1) ||
+                        helper(board, word, i-1, j, m, n, k+1) ||
+                        helper(board, word, i, j-1, m, n, k+1);
 
         board[i][j] = temp;
          return found;
