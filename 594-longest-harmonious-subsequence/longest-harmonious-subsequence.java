a03ae40bc23ec5d6f2652b71efbc35a5f090b0b4
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-04-24 22:51:39 +0530

    Time: 16 ms (89.99%) | Memory: 48.6 MB (8.63%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
@@ -5,22 +5,40 @@ class Solution {
             return 0;
         }
 
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int ele: nums){
-            if(hm.containsKey(ele)){
-                hm.put(ele, hm.get(ele)+1);
-            }
-            else{
-                hm.put(ele,1);
-            }
-        }
+        // HashMap<Integer, Integer> hm = new HashMap<>();
+        // for(int ele: nums){
+        //     if(hm.containsKey(ele)){
+        //         hm.put(ele, hm.get(ele)+1);
+        //     }
+        //     else{
+        //         hm.put(ele,1);
+        //     }
+        // }
 
+        // int maxlen = 0;
+        // Set<Integer> key = hm.keySet();
+        // for(int k: key){
+        //     if(hm.containsKey(k+1)){
+        //         maxlen = Math.max(maxlen, hm.get(k)+hm.get(k+1));
+        //     }
+        // }
+
+        // Slinding Window Approch
+        Arrays.sort(nums);
+        int left = 0;
+        int right = 1;
         int maxlen = 0;
-        Set<Integer> key = hm.keySet();
-        for(int k: key){
-            if(hm.containsKey(k+1)){
-                maxlen = Math.max(maxlen, hm.get(k)+hm.get(k+1));
+        while(left<right  && right<n){
+            if(nums[right]-nums[left] == 1){
+                maxlen = Math.max(maxlen, right-left+1);
             }
+            else if(nums[right]-nums[left] > 1){
+                while(left<n-1 && left < right && nums[left] == nums[left+1]){
+                    left++;
+                }
+                left++;
+            }
+            right++;
         }
 
         return maxlen;
