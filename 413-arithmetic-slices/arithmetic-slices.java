2fbb76aa9f1eba145732b187fe85f7e363344726
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-04-27 22:54:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (10.93%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.java
+++ 413-arithmetic-slices/arithmetic-slices.java
@@ -7,37 +7,35 @@ class Solution {
 
         int left=0;
         int right=0;
-        int subarr = 0;
         int count=0;
-        // while(right<n){
-        //     if(right>=2){
-        //         if( (nums[right-2] - nums[right-1]) != (nums[right-1]-nums[right]) ){
-        //             if(right-left+1 >= 3){
-        //                 subarr = (int)(((long)(right-left)*(right-left-1))/2);
-        //                 count += subarr;
-        //             }
-        //             left = right-1;
-        //         }
-        //     }
-        //     if(right==n-1 && right-left+1 >= 3){
-        //         subarr = (int)(((long)(right-left)*(right-left-1))/2);
-        //         count += subarr;
-        //     }
-        //     right++;
-        // }
-
         while(right<n){
             if(right>=2){
-                if( (nums[right-2] - nums[right-1]) == (nums[right-1]-nums[right])){
-                    subarr++;
-                    count+=subarr;
-                }
-                else{
-                    subarr=0;
+                if( (nums[right-2] - nums[right-1]) != (nums[right-1]-nums[right]) ){
+                    if(right-left >= 3){
+                        count += ((right-left-1)*(right-left-2))/2;
+                    }
+                    left = right-1;
                 }
             }
             right++;
         }
+
+        if(right-left >= 3){
+            count += ((right-left-1)*(right-left-2))/2;
+        }
+
+        // while(right<n){
+        //     if(right>=2){
+        //         if( (nums[right-2] - nums[right-1]) == (nums[right-1]-nums[right])){
+        //             subarr++;
+        //             count+=subarr;
+        //         }
+        //         else{
+        //             subarr=0;
+        //         }
+        //     }
+        //     right++;
+        // }
         return count;
     }
 }
