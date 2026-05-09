920cdde492572d0cf449e6199aebaad9b8a0c907
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-05-10 00:19:59 +0530

    Time: 15 ms (52.89%) | Memory: 48.7 MB (32.85%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n=nums.length;
+        int result[] = new int[n];
+        Stack<Integer> ss = new Stack<>();
+        for(int i=2*n-1; i>=0; i--){
+            int idx = i%n;
+            while(!ss.isEmpty() && ss.peek()<=nums[idx]){
+                ss.pop();
+            }
+
+            if(i<n){
+              result[idx] =  ss.isEmpty() ? -1 : ss.peek();
+            }
+
+            ss.push(nums[idx]);
+        }
+
+        return result;
+    }
+}
