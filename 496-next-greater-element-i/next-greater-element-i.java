d573fddabee84f334e7eb5ab8df89a07129592f8
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-05-09 23:41:31 +0530

    Time: 4 ms (75.05%) | Memory: 45.5 MB (10.50%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,38 +1,72 @@
 class Solution {
+    // public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    //     int n = nums2.length;
+    //     int arr[] = new int[n];
+    //     Stack<Integer> ss = new Stack<>();
+    //     arr[n-1] = -1;
+    //     ss.push(nums2[n-1]);
+    //     for(int i=n-2; i>=0; i--){
+    //         if(ss.size() > 0 && ss.peek() > nums2[i]){
+    //             arr[i] = ss.peek();
+    //         }
+    //         else if(ss.size() > 0 && ss.peek() < nums2[i]){
+    //             while(ss.size() > 0 && ss.peek()<nums2[i]){
+    //                 ss.pop();
+    //             }
+    //         }
+
+    //         if(ss.isEmpty()){
+    //             arr[i]=-1;
+    //         }
+    //         else{
+    //             arr[i]=ss.peek();
+    //         }
+
+    //         ss.push(nums2[i]);
+    //     }
+
+    //     int result[] = new int[nums1.length];
+    //     for(int i=0; i<nums1.length; i++){
+    //         for(int j=0; j<nums2.length; j++){
+    //             if(nums1[i] == nums2[j]){
+    //                 result[i] = arr[j];
+    //                 continue;
+    //             }
+    //         }
+    //     }
+
+    //     return result;
+    // }
+
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int n = nums2.length;
-        int arr[] = new int[n];
         Stack<Integer> ss = new Stack<>();
-        arr[n-1] = -1;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(nums2[n-1], -1);
         ss.push(nums2[n-1]);
         for(int i=n-2; i>=0; i--){
-            if(ss.size() > 0 && ss.peek() > nums2[i]){
-                arr[i] = ss.peek();
+            if(!ss.isEmpty() && ss.peek()>nums2[i]){
+                hm.put(nums2[i], ss.peek());
             }
-            else if(ss.size() > 0 && ss.peek() < nums2[i]){
-                while(ss.size() > 0 && ss.peek()<nums2[i]){
+            else if(!ss.isEmpty() && ss.peek()<nums2[i]){
+                while(!ss.isEmpty() && ss.peek()<nums2[i]){
                     ss.pop();
                 }
             }
 
             if(ss.isEmpty()){
-                arr[i]=-1;
+                hm.put(nums2[i], -1);
             }
             else{
-                arr[i]=ss.peek();
+                hm.put(nums2[i], ss.peek());
             }
 
             ss.push(nums2[i]);
         }
 
         int result[] = new int[nums1.length];
-        for(int i=0; i<nums1.length; i++){
-            for(int j=0; j<nums2.length; j++){
-                if(nums1[i] == nums2[j]){
-                    result[i] = arr[j];
-                    continue;
-                }
-            }
+        for(int i=0; i<result.length; i++){
+            result[i] = hm.get(nums1[i]);
         }
 
         return result;
