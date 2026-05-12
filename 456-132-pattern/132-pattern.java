a3bf0c591c13719c490dc542bafbfd9c34de7296
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-05-13 00:19:13 +0530

    Time: 43 ms (17.77%) | Memory: 98.2 MB (72.01%) - LeetSync

---
+++ 456-132-pattern/132-pattern.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        int n=nums.length;
+        if(n<3){
+            return false;
+        }
+
+        Stack<Integer> ss = new Stack<>();
+        int second = Integer.MIN_VALUE;
+        for(int i=n-1; i>=0; i--){
+            if(ss.isEmpty()){
+                ss.push(nums[i]);
+                continue;
+            }
+            if(nums[i]<second){
+                return true;
+            }
+            else if(nums[i]>=second){
+                while(!ss.isEmpty() && nums[i] > ss.peek()){
+                    second = ss.pop();
+                }
+                ss.push(nums[i]);
+            }
+        }
+
+        return false;
+    }
+}
