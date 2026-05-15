ccf177b7f08c22ae97c96760823dd64151726f48
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-05-15 23:32:42 +0530

    Time: 30 ms (76.00%) | Memory: 48.1 MB (56.51%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -1,22 +1,35 @@
 class Solution {
     public String removeStars(String s) {
         int n=s.length();
-        Stack<Character> ss = new Stack<>();
-        for(int i=0; i<n; i++){
-            char ch = s.charAt(i);
-            if(!ss.isEmpty() && ch == '*'){
-                ss.pop();
-            }
-            else if(ch != '*'){
-                ss.push(ch);
-            }
-        }
+        // Stack<Character> ss = new Stack<>();
+        // for(int i=0; i<n; i++){
+        //     char ch = s.charAt(i);
+        //     if(!ss.isEmpty() && ch == '*'){
+        //         ss.pop();
+        //     }
+        //     else if(ch != '*'){
+        //         ss.push(ch);
+        //     }
+        // }
+
+        // StringBuilder sb = new StringBuilder();
+        // while(!ss.isEmpty()){
+        //     sb.append(ss.pop());
+        // }
+
+        // return sb.reverse().toString();
 
         StringBuilder sb = new StringBuilder();
-        while(!ss.isEmpty()){
-            sb.append(ss.pop());
+        for(int i=0; i<n; i++){
+            char ch = s.charAt(i);
+            if(ch=='*' && sb.length()>0){
+                sb.deleteCharAt(sb.length()-1);
+            }
+            else if(ch !='*'){
+                sb.append(ch);
+            }
         }
 
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
