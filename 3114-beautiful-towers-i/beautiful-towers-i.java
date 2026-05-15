723d1b565e91df76a737f03b1f4f37539f11c618
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-05-16 01:17:20 +0530

    Time: 28 ms (10.06%) | Memory: 47.1 MB (9.50%) - LeetSync

---
+++ 3114-beautiful-towers-i/beautiful-towers-i.java
@@ -0,0 +1,34 @@
+class Solution {
+    long result = 0;
+    public long maximumSumOfHeights(int[] heights) {
+        int n=heights.length;
+        mountainMaxHeight(heights, 0, n);
+        return result;
+    }
+
+    public void mountainMaxHeight(int heights[], int si, int n){
+
+        if(si>=n){
+            return;
+        }
+
+        int arr[] = new int[n];
+        arr[si] = heights[si];
+        for(int i=si-1; i>=0; i--){
+            arr[i] = Math.min(heights[i], arr[i+1]);
+        }
+
+        for(int i=si+1; i<n; i++){
+            arr[i] = Math.min(heights[i], arr[i-1]);
+        }
+
+        long temp = 0;
+        for(int i=0; i<n; i++){
+            temp = temp + (long)(arr[i]);
+        }
+
+        result = Math.max(result, temp);
+
+        mountainMaxHeight(heights, si+1, n);
+    }
+}
