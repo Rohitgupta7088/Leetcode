77f183c0a384e3dbe3a0f233c004311f50e3d76f
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-05-19 00:39:18 +0530

    Time: 3 ms (95.40%) | Memory: 44.5 MB (90.33%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,17 +1,35 @@
 class Solution {
     public int minLength(String s) {
-        int n=s.length();
-        Stack<Character> ss = new Stack<>();
+        //int n=s.length();
+        // Stack<Character> ss = new Stack<>();
 
+        // for(int i=0; i<n; i++){
+        //     char ch = s.charAt(i);
+        //     if( !ss.isEmpty() && ((ss.peek() == 'C' && ch == 'D') || (ss.peek() == 'A' && ch == 'B')) ){
+        //         ss.pop();
+        //         continue;
+        //     }
+        //     ss.push(ch);
+        // }
+
+        // return ss.size();
+
+
+
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<n; i++){
             char ch = s.charAt(i);
-            if( !ss.isEmpty() && ((ss.peek() == 'C' && ch == 'D') || (ss.peek() == 'A' && ch == 'B')) ){
-                ss.pop();
-                continue;
+            if(sb.length()>0){
+                char sblast = sb.charAt(sb.length()-1);
+                if( (sblast == 'A' && ch=='B') || (sblast == 'C' && ch == 'D') ){
+                    sb.deleteCharAt(sb.length()-1);
+                    continue;
+                }
             }
-            ss.push(ch);
+            sb.append(ch);
         }
 
-        return ss.size();
+        return sb.length();
     }
 }
