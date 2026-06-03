7cb2ff4eb52dcd54c5a84b98673457fb044c0ec4
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-06-04 01:06:29 +0530

    Time: 9 ms (51.86%) | Memory: 52.9 MB (56.37%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.java
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.java
@@ -1,35 +1,61 @@
 class Solution {
-    HashSet<List<Integer>> set = new HashSet<>();
+    // HashSet<List<Integer>> set = new HashSet<>();
+    // public List<List<Integer>> findSubsequences(int[] nums) {
+    //     List<Integer> ll = new ArrayList<>();
+    //     List<List<Integer>> bigll = new ArrayList<>();
+    //     helper(nums, 0, bigll, ll);
+    //     return bigll;
+    // }
+
+    // public void helper(int nums[], int idx, List<List<Integer>> bigll, List<Integer> ll){
+    //     if(idx >= nums.length){
+    //         if(ll.size() > 1 && !set.contains(ll)){
+    //             boolean valid = true;
+    //             for(int i=0; i<ll.size(); i++){
+    //                 if(i>0 && ll.get(i-1) > ll.get(i) ){
+    //                     valid = false;
+    //                     break;
+    //                 }
+    //             }
+
+    //             if(valid){
+    //                 bigll.add(new ArrayList<>(ll));
+    //                 set.add(new ArrayList<>(ll));
+    //             }
+    //         }
+
+    //         return;
+    //     }
+
+    //     ll.add(nums[idx]);
+    //     helper(nums, idx+1, bigll, ll);
+    //     ll.remove(ll.size()-1);
+    //     helper(nums, idx+1, bigll, ll);
+    // }
+
+
+
     public List<List<Integer>> findSubsequences(int[] nums) {
-        List<Integer> ll = new ArrayList<>();
         List<List<Integer>> bigll = new ArrayList<>();
+        List<Integer> ll = new ArrayList<>();
         helper(nums, 0, bigll, ll);
         return bigll;
     }
 
     public void helper(int nums[], int idx, List<List<Integer>> bigll, List<Integer> ll){
-        if(idx >= nums.length){
-            if(ll.size() > 1 && !set.contains(ll)){
-                boolean valid = true;
-                for(int i=0; i<ll.size(); i++){
-                    if(i>0 && ll.get(i-1) > ll.get(i) ){
-                        valid = false;
-                        break;
-                    }
-                }
-
-                if(valid){
-                    bigll.add(new ArrayList<>(ll));
-                    set.add(new ArrayList<>(ll));
-                }
-            }
-
-            return;
+        if(ll.size()>=2){
+            bigll.add(new ArrayList<>(ll));
         }
 
-        ll.add(nums[idx]);
-        helper(nums, idx+1, bigll, ll);
-        ll.remove(ll.size()-1);
-        helper(nums, idx+1, bigll, ll);
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=idx; i<nums.length; i++){
+            if(set.contains(nums[i])) continue;
+            if(ll.isEmpty() || nums[i]>=ll.get(ll.size()-1) ){
+                set.add(nums[i]);
+                ll.add(nums[i]);
+                helper(nums, i+1, bigll, ll);
+                ll.remove(ll.size()-1);
+            }
+        }
     }
 }
