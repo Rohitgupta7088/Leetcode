1028b025d9405617223e9c3b0d05e1559dc4d4a3
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-06-14 01:01:01 +0530

    Time: 4 ms (63.73%) | Memory: 43.8 MB (7.25%) - LeetSync

---
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.java
@@ -0,0 +1,46 @@
+class Solution {
+    int ans = Integer.MAX_VALUE;
+    public int distributeCookies(int[] cookies, int k) {
+        List<List<Integer>> bigll = new ArrayList<>();
+        helper(cookies, k, bigll, 0);
+        return ans;
+    }
+
+    public void helper(int nums[], int k, List<List<Integer>> bigll, int idx){
+
+        if(idx >= nums.length){
+            if(bigll.size()  == k){
+                ans = Math.min(ans, calculate(bigll, 0, 0));
+            }
+            return;
+        }
+
+        if(bigll.size() < k){
+            List<Integer> ll = new ArrayList<>();
+            ll.add(nums[idx]);
+            bigll.add(new ArrayList<>(ll));
+            helper(nums, k, bigll, idx+1);
+            bigll.remove(bigll.size()-1);
+        }
+        
+        for(int i=0; i<bigll.size(); i++){
+            bigll.get(i).add(nums[idx]);
+            helper(nums, k, bigll, idx+1);
+            bigll.get(i).remove(bigll.get(i).size()-1);
+        }
+    }
+
+    public int calculate(List<List<Integer>> bigll, int idx, int temp){
+        if(idx >= bigll.size()){
+            return temp;
+        }
+
+        int sum = 0;
+        for(int i=0; i<bigll.get(idx).size(); i++){
+            sum += bigll.get(idx).get(i);
+        }
+
+        temp = Math.max(temp, sum);
+        return calculate(bigll, idx+1, temp);
+    }
+}
