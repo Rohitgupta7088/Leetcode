30ad32b722fe702785cdb7bdf57f18421bae6660
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-06-15 02:52:47 +0530

    Time: 9 ms (58.07%) | Memory: 47.3 MB (70.98%) - LeetSync

---
+++ 4289-valid-binary-strings-with-cost-limit/valid-binary-strings-with-cost-limit.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<String> generateValidStrings(int n, int k) {
+        List<String> ll = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        helper(n, k, ll, sb, 0, 0);
+        return ll;
+    }
+
+    public void helper(int n, int k, List<String> ll, StringBuilder sb, int idx, int cost){
+
+        if(sb.length() == n){
+            if(cost <= k){
+                ll.add(sb.toString());
+            }
+            return;
+        }
+
+        if(sb.length() == 0){
+            sb.append('0');
+            helper(n, k, ll, sb, idx+1, cost);
+            sb.deleteCharAt(sb.length()-1);
+
+            sb.append('1');
+            helper(n, k, ll, sb, idx+1, cost+idx);
+            sb.deleteCharAt(sb.length()-1);
+        }
+
+        if(sb.length() > 0){
+            if(sb.charAt(sb.length()-1) != '1'){
+                sb.append('0');
+                helper(n, k, ll, sb, idx+1, cost);
+                sb.deleteCharAt(sb.length()-1);
+
+                sb.append('1');
+                helper(n, k, ll, sb, idx+1, cost+idx);
+                sb.deleteCharAt(sb.length()-1);
+            }
+            else{
+                sb.append('0');
+                helper(n, k, ll, sb, idx+1, cost);
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }
+    }
+}
