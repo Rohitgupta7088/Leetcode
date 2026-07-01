b7c46bcc2593a93e2f98465e81fd83db4dabae06
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-07-01 23:56:30 +0530

    Time: 488 ms (5.03%) | Memory: 46.9 MB (18.80%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
@@ -1,36 +1,80 @@
 class Solution {
+    // int count = 0;
+    // public int findPairs(int[] nums, int k) {
+    //     List<Integer> ll = new ArrayList<>();
+    //     Arrays.sort(nums);
+    //     for(int i=nums.length-1; i>0; i--){
+    //         if(!ll.contains(nums[i])){
+    //             ll.add(nums[i]);
+    //             int idx = helper(nums, 0, i-1, nums[i]-k);
+    //             if(idx == -1) continue;
+    //             else count++;
+    //         }
+    //     }
+
+    //     return count;
+    // }
+
+    // public int helper(int nums[], int si, int ei, int target){
+    //     if(si > ei){
+    //         return -1;
+    //     }
+
+    //     int mid = si+(ei-si)/2;
+
+    //     if(nums[mid] == target){
+    //         return mid;
+    //     }
+
+    //     if(nums[mid] < target){
+    //         return helper(nums, mid+1, ei, target);
+    //     }
+    //     else{
+    //         return helper(nums, si, mid-1, target);
+    //     }
+    // }
+
+
+
+
+
     int count = 0;
     public int findPairs(int[] nums, int k) {
-        List<Integer> ll = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=nums.length-1; i>0; i--){
-            if(!ll.contains(nums[i])){
-                ll.add(nums[i]);
-                int idx = helper(nums, 0, i-1, nums[i]-k);
-                if(idx == -1) continue;
-                else count++;
+
+        int left =  0;
+        int right = 1;
+
+        List<Integer> ll = new ArrayList<>();
+        while(right < nums.length && left<nums.length){
+            if( !ll.contains(nums[left]) ){
+
+                if(left == right){
+                    right++;
+                    continue;
+                }
+
+                if(nums[right]-nums[left] == k){
+                    ll.add(nums[left]);
+                    count++;
+                    right++;
+                    left++;
+                    continue;
+                }
+
+                if(nums[right]-nums[left] < k){
+                    right++;
+                }
+                else if(nums[right]-nums[left] >k){
+                    left++;
+                }
+            }
+            else{
+                left++;
+                continue;
             }
         }
 
         return count;
-    }
-
-    public int helper(int nums[], int si, int ei, int target){
-        if(si > ei){
-            return -1;
-        }
-
-        int mid = si+(ei-si)/2;
-
-        if(nums[mid] == target){
-            return mid;
-        }
-
-        if(nums[mid] < target){
-            return helper(nums, mid+1, ei, target);
-        }
-        else{
-            return helper(nums, si, mid-1, target);
-        }
     }
 }
