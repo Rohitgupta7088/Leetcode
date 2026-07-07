4337aa70ff8fd4d97f43b1f3f548022129117872
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-07-08 01:46:53 +0530

    Time: 2 ms (97.61%) | Memory: 45 MB (28.85%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,39 +1,51 @@
 class Solution {
-    public boolean checkIfExist(int[] arr) {
-        Arrays.sort(arr);
+    // public boolean checkIfExist(int[] arr) {
+    //     Arrays.sort(arr);
 
-        for(int i=arr.length-1; i>=0; i--){
-            if(arr[i]>=0){
-                if(helper(arr, 0, i-1, arr[i])){
-                    return true;
-                }
+    //     for(int i=arr.length-1; i>=0; i--){
+    //         if(arr[i]>=0 && helper(arr, 0, i-1, arr[i])){
+    //             return true;
+    //         }
+    //         else if(arr[i]<0 && helper(arr, i+1, arr.length-1, arr[i])){
+    //             return true;
+    //         }
+    //     }
+
+    //     return false;
+    // }
+
+    // public boolean helper(int arr[], int si, int ei, int target){
+    //     if(si>ei){
+    //         return false;
+    //     }
+
+    //     int mid=si+(ei-si)/2;
+
+    //     if((long)2*arr[mid] == (long)target){
+    //         return true;
+    //     }
+
+    //     if((long)2*arr[mid] < (long)target){
+    //         return helper(arr, mid+1, ei, target);
+    //     }
+    //     else{
+    //         return helper(arr, si, mid-1, target);
+    //     }
+    // }
+
+
+    public boolean checkIfExist(int[] arr) {
+        Set<Integer> ss = new HashSet<>();
+
+        for(int num: arr){
+            if(ss.contains(2*num) || (num%2 == 0 && ss.contains(num/2))){
+                return true;
             }
             else{
-                if(helper(arr, i+1, arr.length-1, arr[i])){
-                    return true;
-                }
+                ss.add(num);
             }
         }
 
         return false;
-    }
-
-    public boolean helper(int arr[], int si, int ei, int target){
-        if(si>ei){
-            return false;
-        }
-
-        int mid=si+(ei-si)/2;
-
-        if((long)2*arr[mid] == (long)target){
-            return true;
-        }
-
-        if((long)2*arr[mid] < (long)target){
-            return helper(arr, mid+1, ei, target);
-        }
-        else{
-            return helper(arr, si, mid-1, target);
-        }
     }
 }
