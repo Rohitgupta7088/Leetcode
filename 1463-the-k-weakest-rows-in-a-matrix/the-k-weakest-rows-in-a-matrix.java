b307e2859939b78cb538215d99d650b2638a6eb8
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-07-08 00:05:11 +0530

    Time: 2 ms (92.74%) | Memory: 46.3 MB (92.60%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -3,11 +3,7 @@ class Solution {
         int arr[][] = new int[mat.length][2];
 
         for(int i=0; i<mat.length; i++){
-            int sum = 0;
-            for(int j=0; j<mat[0].length; j++){
-                sum += mat[i][j];
-            }
-            arr[i][0] = sum;
+            arr[i][0] = helpercount(mat[i], 0, mat[0].length-1);
             arr[i][1] = i;
         }
 
@@ -19,5 +15,20 @@ class Solution {
         }
 
         return ans;
+    }
+
+    public int helpercount(int matrow[], int si, int ei){
+        if(si>ei){
+            return si;
+        }
+
+        int mid = si+(ei-si)/2;
+
+        if(matrow[mid] == 1){
+            return helpercount(matrow, mid+1, ei);
+        }
+        else{
+            return helpercount(matrow, si, mid-1);
+        }
     }
 }
