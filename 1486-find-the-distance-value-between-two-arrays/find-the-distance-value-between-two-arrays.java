51931ef29e95880ce243e5913e3a6fea713e0a17
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-07-09 00:22:29 +0530

    Time: 3 ms (97.99%) | Memory: 46.1 MB (79.33%) - LeetSync

--- 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.java
+++ 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.java
@@ -1,32 +1,49 @@
 class Solution {
+    // public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+    //     Arrays.sort(arr2);
+    //     int count = 0;
+    //     for(int i=0; i<arr1.length; i++){
+    //         if(!helper(arr2, 0, arr2.length-1, arr1[i], d)){
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
+
+    // public boolean helper(int arr2[], int si, int ei, int val, int d){
+
+    //     while(si<=ei){
+    //         int mid = si+(ei-si)/2;
+    //         int tempdis = Math.abs(val-arr2[mid]);
+    //         if(tempdis<=d){
+    //             return true;
+    //         }
+
+    //         if(arr2[mid]<val){
+    //             si=mid+1;
+    //         }
+    //         else{
+    //             ei = mid-1;
+    //         }
+    //     }
+
+    //     return false;
+    // }
+
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
-        Arrays.sort(arr2);
-        int count = 0;
+        int ans = 0;
         for(int i=0; i<arr1.length; i++){
-            if(!helper(arr2, 0, arr2.length-1, arr1[i], d)){
-                count++;
+            int count = 0;
+            for(int j=0; j<arr2.length; j++){
+                if(Math.abs(arr2[j]-arr1[i])<=d){
+                    count++;
+                    break;
+                }
+            }
+            if(count == 0){
+                ans++;
             }
         }
-        return count;
-    }
-
-    public boolean helper(int arr2[], int si, int ei, int val, int d){
-
-        while(si<=ei){
-            int mid = si+(ei-si)/2;
-            int tempdis = Math.abs(val-arr2[mid]);
-            if(tempdis<=d){
-                return true;
-            }
-
-            if(arr2[mid]<val){
-                si=mid+1;
-            }
-            else{
-                ei = mid-1;
-            }
-        }
-
-        return false;
+        return ans;
     }
 }
