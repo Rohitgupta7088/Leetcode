12382184e0e4eb9721393f90f0fa7d4225a5e272
Rohitgupta7088 <rohitgupta31298@gmail.com>
2026-07-10 23:40:45 +0530

    Time: 4 ms (50.49%) | Memory: 46.1 MB (96.60%) - LeetSync

--- 1232-sum-of-mutated-array-closest-to-target/sum-of-mutated-array-closest-to-target.java
+++ 1232-sum-of-mutated-array-closest-to-target/sum-of-mutated-array-closest-to-target.java
@@ -1,63 +1,104 @@
 class Solution {
+
     public int findBestValue(int[] arr, int target) {
-        Arrays.sort(arr);
         int low = 0;
-        int high = arr[arr.length-1];
+        int high = 0;
 
-        int prefixsum[] = new int[arr.length+1];
-        prefixsum[0] = 0;
-
-        for(int i=0; i<arr.length; i++){
-            prefixsum[i+1] = prefixsum[i]+arr[i];
+        for(int num: arr){
+            high = Math.max(high, num);
         }
 
         int dis = Integer.MAX_VALUE;
-        int ans = high;
+        int ans = 0;
+
         while(low<=high){
             int mid = low+(high-low)/2;
 
-            int idx = helper(arr, 0, arr.length-1, mid);
-
-            int sum = prefixsum[idx]+(arr.length-idx)*mid;
-
-            int tempdiff =  Math.abs(sum-target);
-
-            if(tempdiff == 0){
-                return mid;
+            int sum = 0;
+            for(int num: arr){
+                sum += Math.min(mid, num);
             }
+
+            int tempdiff = Math.abs(sum-target);
 
             if(dis>tempdiff){
                 ans = mid;
                 dis = tempdiff;
             }
 
-            if(dis == tempdiff && mid<=ans){
+            if(dis==tempdiff && mid<=ans){
                 ans = mid;
             }
-            
+
             if(sum<target){
                 low = mid+1;
             }
             else{
                 high = mid-1;
             }
-
         }
+
         return ans;
     }
+    // public int findBestValue(int[] arr, int target) {
+    //     Arrays.sort(arr);
+    //     int low = 0;
+    //     int high = arr[arr.length-1];
 
-    public int helper(int arr[], int si, int ei, int target){
-        if(si>ei){
-            return si;
-        }
+    //     int prefixsum[] = new int[arr.length+1];
+    //     prefixsum[0] = 0;
 
-        int mid = si+(ei-si)/2;
+    //     for(int i=0; i<arr.length; i++){
+    //         prefixsum[i+1] = prefixsum[i]+arr[i];
+    //     }
 
-        if(arr[mid]<=target){
-            return helper(arr, mid+1, ei, target);
-        }
-        else{
-            return helper(arr, si, mid-1, target);
-        }
-    }
+    //     int dis = Integer.MAX_VALUE;
+    //     int ans = high;
+    //     while(low<=high){
+    //         int mid = low+(high-low)/2;
+
+    //         int idx = helper(arr, 0, arr.length-1, mid);
+
+    //         int sum = prefixsum[idx]+(arr.length-idx)*mid;
+
+    //         int tempdiff =  Math.abs(sum-target);
+
+    //         if(tempdiff == 0){
+    //             return mid;
+    //         }
+
+    //         if(dis>tempdiff){
+    //             ans = mid;
+    //             dis = tempdiff;
+    //         }
+
+    //         if(dis == tempdiff && mid<=ans){
+    //             ans = mid;
+    //         }
+            
+    //         if(sum<target){
+    //             low = mid+1;
+    //         }
+    //         else{
+    //             high = mid-1;
+    //         }
+
+    //     }
+    //     return ans;
+    // }
+
+    // public int helper(int arr[], int si, int ei, int target){
+    //     if(si>ei){
+    //         return si;
+    //     }
+
+    //     int mid = si+(ei-si)/2;
+
+    //     if(arr[mid]<=target){
+    //         return helper(arr, mid+1, ei, target);
+    //     }
+    //     else{
+    //         return helper(arr, si, mid-1, target);
+    //     }
+    // }
 }
